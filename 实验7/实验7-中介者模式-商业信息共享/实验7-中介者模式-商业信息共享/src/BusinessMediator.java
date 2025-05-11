8028f1e473f4db39b99fce9544f41a78a6b4d23a
kannoy <zuzhenhuang365@gmail.com>
2025-05-11 15:41:53 +0800

    修改实验七的警告

--- 实验7/实验7-中介者模式-商业信息共享/实验7-中介者模式-商业信息共享/src/BusinessMediator.java
+++ 实验7/实验7-中介者模式-商业信息共享/实验7-中介者模式-商业信息共享/src/BusinessMediator.java
@@ -1,6 +1,6 @@
 
-import javax.swing.*;
-import java.awt.Color;
+// import javax.swing.*;
+// import java.awt.Color;
 
 
 /*======================================*/
--- 实验7/实验7-中介者模式-商业信息共享/实验7-中介者模式-商业信息共享/src/HotelGUI.java
+++ 实验7/实验7-中介者模式-商业信息共享/实验7-中介者模式-商业信息共享/src/HotelGUI.java
@@ -2,90 +2,88 @@
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
 public class HotelGUI extends JFrame {
-  private JSplitPane  bigSplitPane;
-  private JScrollPane showInfoPane;
-  private JPanel btnPanel;
-  private JEditorPane editorPane;
-  private JComboBox nationality;
-  private JLabel lblName, lblID, nation;
-  private JTextField nameText, iDText;
-  private String cusName;
-  private String cusID;
-  private String cusNation;
-  private JTextArea display;
-  private Dimension size = new Dimension(600, 210);
-  private static int divider = 370;
-  public static final String HOTEL = "hotel";
-  public static final String SUBMIT = "Submit";
-  public static final String EXIT = "Exit";
+   private JSplitPane bigSplitPane;
+   private JScrollPane showInfoPane;
+   private JPanel btnPanel;
+   // private JEditorPane editorPane;
+   private JComboBox<String> nationality;
+   private JLabel lblName, lblID, nation;
+   private JTextField nameText, iDText;
+   private String cusName;
+   private String cusID;
+   private String cusNation;
+   private JTextArea display;
+   private Dimension size = new Dimension(600, 210);
+   private static int divider = 370;
+   public static final String HOTEL = "hotel";
+   public static final String SUBMIT = "Submit";
+   public static final String EXIT = "Exit";
 
-  private BusinessMediator bMediator;
-  private ArrayList<String> cusNames;
-  private ArrayList<String> candidateCusNames;
+   private BusinessMediator bMediator;
+   private ArrayList<String> cusNames;
+   private ArrayList<String> candidateCusNames;
 
-  public HotelGUI(BusinessMediator bMdtr) {
-     super("Mediator Pattern- Hotel ");
-     bMediator = bMdtr;
-     setUpChoicePanel();
-     setUpScrollPanes();
-     cusNames = new ArrayList<String>();
-     candidateCusNames = new ArrayList<String>();
-     bMediator.registerHotelGUI(this);
+   public HotelGUI(BusinessMediator BusinessMediator) {
+      super("Mediator Pattern- Hotel ");
+      bMediator = BusinessMediator;
+      setUpChoicePanel();
+      setUpScrollPanes();
+      cusNames = new ArrayList<String>();
+      candidateCusNames = new ArrayList<String>();
+      bMediator.registerHotelGUI(this);
    }
 
-  private void setUpChoicePanel() {
+   private void setUpChoicePanel() {
 
       nameText = new JTextField(20);
       iDText = new JTextField(20);
 
-      nationality = new JComboBox();
-	  nationality.addItem("China");
-	  nationality.addItem( "USA");
-	  nationality.addItem("England");
-	  nationality.addItem( "France");
-	  nationality.addItem("Germany");
-	  nationality.addItem( "Russia");
-	  nationality.addItem("Japan");
-	  nationality.addItem( "Korea");
+      nationality = new JComboBox<String>();
+      nationality.addItem("China");
+      nationality.addItem("USA");
+      nationality.addItem("England");
+      nationality.addItem("France");
+      nationality.addItem("Germany");
+      nationality.addItem("Russia");
+      nationality.addItem("Japan");
+      nationality.addItem("Korea");
 
-	  lblName = new JLabel("Customer Name:");
-	  lblID = new JLabel("Customer ID:");
-	  nation = new JLabel("Nationality:");
+      lblName = new JLabel("Customer Name:");
+      lblID = new JLabel("Customer ID:");
+      nation = new JLabel("Nationality:");
 
-	  //Create the open button
-	  JButton submitBtn = new JButton(SUBMIT);
-	  submitBtn.setMnemonic(KeyEvent.VK_S);
-	  JButton exitButton = new JButton(EXIT);
-	  exitButton.setMnemonic(KeyEvent.VK_X);
+      // Create the open button
+      JButton submitBtn = new JButton(SUBMIT);
+      submitBtn.setMnemonic(KeyEvent.VK_S);
+      JButton exitButton = new JButton(EXIT);
+      exitButton.setMnemonic(KeyEvent.VK_X);
 
-	  ButtonListener btnListener = new ButtonListener();
+      ButtonListener btnListener = new ButtonListener();
 
-	  // add action Listener
-	  submitBtn.addActionListener(btnListener);
-	  exitButton.addActionListener(btnListener);
+      // add action Listener
+      submitBtn.addActionListener(btnListener);
+      exitButton.addActionListener(btnListener);
 
-	  btnPanel = new JPanel();
+      btnPanel = new JPanel();
 
-	  //------------------------------------------------
-	  GridBagLayout gridbag = new GridBagLayout();
-	  btnPanel.setLayout(gridbag);
-	  GridBagConstraints gbc = new GridBagConstraints();
+      // ------------------------------------------------
+      GridBagLayout gridBag = new GridBagLayout();
+      btnPanel.setLayout(gridBag);
+      GridBagConstraints gbc = new GridBagConstraints();
 
-	  btnPanel.add(lblName);
-	  btnPanel.add(nameText);
-	  btnPanel.add(lblID);
-	  btnPanel.add(iDText);
+      btnPanel.add(lblName);
+      btnPanel.add(nameText);
+      btnPanel.add(lblID);
+      btnPanel.add(iDText);
       btnPanel.add(nation);
-	  btnPanel.add(nationality);
-	  btnPanel.add(submitBtn);
-	  btnPanel.add(exitButton);
+      btnPanel.add(nationality);
+      btnPanel.add(submitBtn);
+      btnPanel.add(exitButton);
 
       gbc.insets.top = 5;
       gbc.insets.bottom = 5;
@@ -94,136 +92,144 @@ public class HotelGUI extends JFrame {
 
       gbc.gridx = 0;
       gbc.gridy = 0;
-      gridbag.setConstraints(lblName, gbc);
+      gridBag.setConstraints(lblName, gbc);
       gbc.gridx = 1;
       gbc.gridy = 0;
-      gridbag.setConstraints(nameText, gbc);
+      gridBag.setConstraints(nameText, gbc);
 
       gbc.gridx = 0;
       gbc.gridy = 1;
-      gridbag.setConstraints(lblID, gbc);
+      gridBag.setConstraints(lblID, gbc);
       gbc.gridx = 1;
       gbc.gridy = 1;
-      gridbag.setConstraints(iDText, gbc);
+      gridBag.setConstraints(iDText, gbc);
 
       gbc.gridx = 0;
-	  gbc.gridy = 2;
-      gridbag.setConstraints(nation, gbc);
+      gbc.gridy = 2;
+      gridBag.setConstraints(nation, gbc);
       gbc.gridx = 1;
-	  gbc.gridy = 2;
-      gridbag.setConstraints(nationality, gbc);
+      gbc.gridy = 2;
+      gridBag.setConstraints(nationality, gbc);
 
       gbc.insets.left = 2;
       gbc.insets.right = 2;
       gbc.insets.top = 15;
       gbc.gridx = 0;
       gbc.gridy = 5;
-      gridbag.setConstraints(submitBtn, gbc);
+      gridBag.setConstraints(submitBtn, gbc);
       gbc.anchor = GridBagConstraints.WEST;
       gbc.gridx = 1;
       gbc.gridy = 5;
-      gridbag.setConstraints(exitButton, gbc);
-      //-----------------------------------------------
+      gridBag.setConstraints(exitButton, gbc);
+      // -----------------------------------------------
    }
 
    private void setUpScrollPanes() {
-   	  Border raisedbevel = BorderFactory.createRaisedBevelBorder();
+      Border raisedBevel = BorderFactory.createRaisedBevelBorder();
 
-  	  editorPane = new JEditorPane();
+      // editorPane = new JEditorPane();
 
       display = new JTextArea(15, 15);
-  	  showInfoPane = new JScrollPane(display);
+      display.setBorder(raisedBevel);
+      showInfoPane = new JScrollPane(display);
 
-  	  bigSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, btnPanel,showInfoPane);
-  	  bigSplitPane.setDividerLocation(divider);
+      bigSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, btnPanel, showInfoPane);
+      bigSplitPane.setDividerLocation(divider);
 
       getContentPane().add(bigSplitPane);
-  	  setSize(size);
+      setSize(size);
       setVisible(true);
    }
 
-   public void setCusName(){
+   public void setCusName() {
       cusName = nameText.getText();
    }
-   public void setCusID(){
+
+   public void setCusID() {
       cusID = iDText.getText();
    }
-   public void setCusNation(){
-      cusNation = (String)nationality.getSelectedItem();
+
+   public void setCusNation() {
+      cusNation = (String) nationality.getSelectedItem();
    }
 
-   public String getCusName(){
+   public String getCusName() {
       return cusName;
    }
-   public String getCusID(){
+
+   public String getCusID() {
       return cusID;
    }
-   public String getCusNation(){
+
+   public String getCusNation() {
       return cusNation;
    }
-   public void addCustomer(String cus){
+
+   public void addCustomer(String cus) {
       cusNames.add(cus);
    }
-   public void addCandidateCustomer(String cus){
+
+   public void addCandidateCustomer(String cus) {
       candidateCusNames.add(cus);
    }
-   public void writeReservedCusToDB(){
-      String s;
-      Iterator e = cusNames.iterator();
-      while (e.hasNext()){
-         s = (String) e.next();
-         //try to write data to DB
-      }
-   }
-   public void writeCandidateCusToDB(){
-      String s;
-      Iterator e = cusNames.iterator();
-      while (e.hasNext()){
-         s = (String) e.next();
-         //try to write data to DB
-      }
-   }
 
+   public void writeReservedCusToDB() {
+      Iterator<String> e = cusNames.iterator();
+      while (e.hasNext()) {
+         String s = (String) e.next();
+         System.out.println("Writing to DB (reserved): " + s);
+         // try to write data to DB
+      }
+   }
+   // public void writeCandidateCusToDB(){
+   // String s;
+   // Iterator<String> e = cusNames.iterator();
+   // while (e.hasNext()){
+   // s = (String) e.next();
+   // //try to write data to DB
+   // }
+   // }
 
    class ButtonListener implements ActionListener {
       public void actionPerformed(ActionEvent ae) {
 
-		if (ae.getActionCommand().equals(EXIT)) {
-		   System.exit(1);
-		}
-		if (ae.getActionCommand().equals(SUBMIT)) {
-           setCusName();
-		   setCusID();
-           setCusNation();
-           String nm = getCusName();
-           String id = getCusID();
-           String na = getCusNation();
+         if (ae.getActionCommand().equals(EXIT)) {
+            System.exit(1);
+         }
+         if (ae.getActionCommand().equals(SUBMIT)) {
+            setCusName();
+            setCusID();
+            setCusNation();
+            String nm = getCusName();
+            String id = getCusID();
+            String na = getCusNation();
 
-           displayInfoToScreen("\nHotel reserved:\n" + nm);
-           displayInfoToScreen("\n" + id );
-           displayInfoToScreen("\n" + na );
+            displayInfoToScreen("\nHotel reserved:\n" + nm);
+            displayInfoToScreen("\n" + id);
+            displayInfoToScreen("\n" + na);
 
-           String hotelCus = "\n\nCustomer check in hotel:";
-           String cusInfo = hotelCus+"\n Name: " + nm + "\n ID: " + id + "\n Nationality: " + na;
+            String hotelCus = "\n\nCustomer check in hotel:";
+            String cusInfo = hotelCus + "\n Name: " + nm + "\n ID: " + id + "\n Nationality: " + na;
 
-           addCustomer(cusInfo);
-           bMediator.addAllCandidateCus(HOTEL,cusInfo);
-           bMediator.updateALlGuis(HOTEL,cusInfo);
-        }
+            addCustomer(cusInfo);
+            bMediator.addAllCandidateCus(HOTEL, cusInfo);
+            bMediator.updateALlGuis(HOTEL, cusInfo);
+         }
       }
-  }
+   }
 
-  public void displayInfoToScreen(String txt) {
-        display.append(txt);
-  }
+   public void displayInfoToScreen(String txt) {
+      display.append(txt);
+   }
 }
 
-/* PRINT all the listed customers
-
-   String s;
-       Iterator e = nums.iterator();
-       while (e.hasNext()) {
-         s = (String) e.next();
-         System.out.println(s);
-       }
-*/
+/*
+ * PRINT all the listed customers
+ * 
+ * String s;
+ * Iterator e = nums.iterator();
+ * while (e.hasNext()) {
+ * s = (String) e.next();
+ * System.out.println(s);
+ * }
+ */
